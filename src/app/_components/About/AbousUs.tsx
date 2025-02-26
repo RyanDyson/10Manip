13b9664d4e76a0485246fedbfa1b8b91e53e41d0
Ryan Dyson Darmawan <dysonryan6@gmail.com>
2025-02-26 18:29:05 +0800

    maybe ill continue it someday

--- src/app/_components/About/AbousUs.tsx
+++ src/app/_components/About/AbousUs.tsx
@@ -1,12 +1,72 @@
+"use client";
+
 import { DisplayText } from "../DisplayText";
+import All from "@/../public/Members/all.png";
+import Image from "next/image";
+import Ryan from "@/../public/Members/Ryan.png";
+// import Keisha from "@/../public/Members/keisha.png";
+// import Cay from "@/../public/Members/Cayleen.png";
+// import Hans from "@/../public/Members/Hans.png";
+// import Bryan from "@/../public/Members/Bryan.png";
+// import Fillbert from "@/../public/Members/Fillbert.png";
+// import Jason from "@/../public/Members/Jason.png";
+// import Rachel from "@/../public/Members/Rachel.png";
+// import Chris from "@/../public/Members/Chris.png";
+// import Leon from "@/../public/Members/Leon.png";
+import useMeasure from "react-use-measure";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 export function AboutUs() {
+  const [ref, bounds] = useMeasure();
+  const [memberHover, setMemberHover] = useState<number | null>(null);
+  //offset -> when vw is 1026
+
   return (
     <div className="px-8 md:px-32 py-8 bg-neutral-950 flex flex-col gap-y-4 items-center">
       <DisplayText className="text-9xl w-full text-center">
         ABOUT US
       </DisplayText>
       <p>Just a group of friends, interested in CS</p>
+      <div className="h-full relative w-screen">
+        <Image
+          src={All}
+          ref={ref}
+          alt="orion pax, your sacrafice for the greater good has proven you worthy in the eyes of primus. He entrusts you the future of cybertron and the matrix of leadership"
+          className="object-cover rounded-lg absolute z-20 transition-all duration-100"
+          quality={100}
+          style={
+            memberHover != null
+              ? { filter: "grayscale(1)" }
+              : { filter: "grayscale(0)" }
+          }
+        />
+        <motion.div
+          className="absolute z-30"
+          style={{
+            left: (290 / 1062) * bounds.width,
+            top: (300 / 891) * bounds.height,
+          }}
+          onMouseEnter={() => setMemberHover(0)}
+          onMouseLeave={() => setMemberHover(null)}
+          animate={
+            memberHover == 0
+              ? { height: bounds.height * 0.8 }
+              : { height: bounds.height * 0.65 }
+          }
+        >
+          <Image src={Ryan} alt="Ryan" className="h-full object-contain" />
+        </motion.div>
+        {/* <Image src={Keisha} alt="Keisha" className="absolute" />
+        <Image src={Cay} alt="Cayleen" className="absolute" />
+        <Image src={Hans} alt="Hans" className="absolute" />
+        <Image src={Bryan} alt="Bryan" className="absolute" />
+        <Image src={Fillbert} alt="Fillbert" className="absolute" />
+        <Image src={Jason} alt="Jason" className="absolute" />
+        <Image src={Rachel} alt="Rachel" className="absolute" />
+        <Image src={Chris} alt="Chris" className="absolute" />
+        <Image src={Leon} alt="Leon" className="absolute" /> */}
+      </div>
     </div>
   );
 }
--- src/app/_components/About/Members.ts
+++ src/app/_components/About/Members.ts
@@ -1,8 +1,24 @@
+import Ryan from "@/../public/Members/Ryan.png";
+import Keisha from "@/../public/Members/keisha.png";
+import Cay from "@/../public/Members/Cayleen.png";
+import Hans from "@/../public/Members/Hans.png";
+import Bryan from "@/../public/Members/Bryan.png";
+import Fillbert from "@/../public/Members/Fillbert.png";
+import Jason from "@/../public/Members/Jason.png";
+import Rachel from "@/../public/Members/Rachel.png";
+import Chris from "@/../public/Members/Chris.png";
+import Leon from "@/../public/Members/Leon.png";
+
 export const Members = {
   Ryan: {
     name: "Ryan Dyson Darmawan",
     title: "Full stack developer",
     bio: "lorem ipsum",
+    image: Ryan,
+    offset: {
+      x: 340,
+      y: 310,
+    },
     personal: "https://portfolio-ryan-six.vercel.app/",
     github: "https://github.com/RyanDyson",
   },
@@ -10,14 +26,108 @@ export const Members = {
     name: "Hans Yang Ericson",
     title: "",
     bio: "lorem ipsum",
+    image: Hans,
+    offset: {
+      x: 0,
+      y: 0,
+    },
     personal: "",
+    github: "",
   },
-  Cayleen: {},
-  Christ: {},
-  keshia: {},
-  Rachel: {},
-  Jason: {},
-  Fillbert: {},
-  Leon: {},
-  Bryan: {},
+  Cayleen: {
+    name: "Cayleen",
+    title: "",
+    bio: "lorem ipsum",
+    image: Cay,
+    offset: {
+      x: 0,
+      y: 0,
+    },
+    personal: "",
+    github: "",
+  },
+  Christ: {
+    name: "Christ",
+    title: "",
+    bio: "lorem ipsum",
+    image: Chris,
+    offset: {
+      x: 0,
+      y: 0,
+    },
+    personal: "",
+    github: "",
+  },
+  keshia: {
+    name: "Keisha",
+    title: "",
+    bio: "lorem ipsum",
+    image: Keisha,
+    offset: {
+      x: 0,
+      y: 0,
+    },
+    personal: "",
+    github: "",
+  },
+  Rachel: {
+    name: "Rachel",
+    title: "",
+    bio: "lorem ipsum",
+    image: Rachel,
+    offset: {
+      x: 0,
+      y: 0,
+    },
+    personal: "",
+    github: "",
+  },
+  Jason: {
+    name: "Jason",
+    title: "",
+    bio: "lorem ipsum",
+    image: Jason,
+    offset: {
+      x: 0,
+      y: 0,
+    },
+    personal: "",
+    github: "",
+  },
+  Fillbert: {
+    name: "Fillbert",
+    title: "",
+    bio: "lorem ipsum",
+    image: Fillbert,
+    offset: {
+      x: 0,
+      y: 0,
+    },
+    personal: "",
+    github: "",
+  },
+  Leon: {
+    name: "Leon",
+    title: "",
+    bio: "lorem ipsum",
+    image: Leon,
+    offset: {
+      x: 0,
+      y: 0,
+    },
+    personal: "",
+    github: "",
+  },
+  Bryan: {
+    name: "Bryan",
+    title: "",
+    bio: "lorem ipsum",
+    image: Bryan,
+    offset: {
+      x: 0,
+      y: 0,
+    },
+    personal: "",
+    github: "",
+  },
 };
